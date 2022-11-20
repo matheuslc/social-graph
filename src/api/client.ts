127af07d51e39c0329c674860db3485f7859eff5
Matheus Carmo <matheus.carmo@rocket.chat>
2022-11-20 10:52:31 -0300

    feat: add env vars

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const apiURL = import.meta.env.VITE_API_URL || 'http://localhost:3010'
+
 export default axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3010'
+    baseURL: apiURL
 })
